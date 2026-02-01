3e31e43656e8e563624ab0ae7ab7cd15f9135459
grehochka82 <lobodina0607@mail.ru>
2026-02-01 12:21:06 +0300

    Коммит 8 — добавление группы и даты

--- server/Program.cs
+++ server/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main()
     {
         Console.WriteLine("Приветствие");
+        Console.WriteLine("ФИО: Иванов Иван Иванович");
+         Console.WriteLine("Группа: ИСРПО-1-23");
+        Console.WriteLine($"Текущая дата и время: {DateTime.Now}");
+        
     }
 }
