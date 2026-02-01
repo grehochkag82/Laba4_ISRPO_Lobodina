3c3f2c96e86faa369e876fdd5d8af05aa1e4289a
grehochka82 <lobodina0607@mail.ru>
2026-02-01 12:34:24 +0300

    Коммит 10 — добавление логики выбора

--- server/Program.cs
+++ server/Program.cs
@@ -6,14 +6,37 @@ class Program
     {
         Console.WriteLine("Приветствие");
         Console.WriteLine("ФИО: Лободина Вероника Александровна");
-        Console.WriteLine("Группа: ИСП-233");
+        Console.WriteLine("Группа: ИСП-232");
         Console.WriteLine($"Текущая дата и время: {DateTime.Now}");
  
-        string choice = Console.ReadLine();
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("\nМеню:");
+            Console.WriteLine("1 — Показать ФИО");
+            Console.WriteLine("2 — Показать группу");
+            Console.WriteLine("3 — Показать дату");
+            Console.WriteLine("4 — Выход");
+             string choice = Console.ReadLine();
             
             switch (choice)
             {
-               
+                case "1":
+                    Console.WriteLine("Иванов Иван Иванович");
+                    break;
+                case "2":
+                    Console.WriteLine("ИСРПО-1-23");
+                    break;
+                case "3":
+                    Console.WriteLine(DateTime.Now);
+                    break;
+                case "4":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор");
+                    break;
             }
+        }
     }
 }
